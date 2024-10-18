b030fec17d680b415571f8db1d64793deab8e5ab
ydriss foko <300141534@monboreal.ca>
2024-10-18 16:42:38 -0400

    ajout de apppaiement

---
+++ AppPaiements/Paiement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+
+    public class Paiements
+    {
+        public string Description { get; set; }
+        public int Montant { get; set; }
+
+        public Paiements(string description, int montant)
+        {
+            Description = description;
+            Montant = montant;
+        }
+
+       
+
+        public virtual void AfficherDetails()
+        {
+            Console.WriteLine($"motif: {Description} montant {Montant}");
+        }
+
+    }
+}
