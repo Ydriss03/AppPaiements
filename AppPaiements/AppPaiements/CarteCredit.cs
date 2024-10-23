3cce2b65a4c0ba71dc03043ddb53b485150709bc
ydriss foko <300141534@monboreal.ca>
2024-10-23 13:23:40 -0400

    ajout de la classe CarteCredit

---
+++ AppPaiements/AppPaiements/CarteCredit.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+    public class CarteCredit:Paiement
+    {
+        public int NumeroCarte { get; set; }
+        public CarteCredit(double montant, string description, int carte) : base(montant,description)
+        {
+            Montant= montant;  
+            Description= description;
+            NumeroCarte = carte;
+        }
+
+        public override void AfficherDetails()
+        {
+            Console.WriteLine($"Montant: {Montant}, Description: {Description}, Carte:{NumeroCarte}");
+        }
+    }
+}
+
--- AppPaiements/AppPaiements/Program.cs
+++ AppPaiements/AppPaiements/Program.cs
@@ -1,7 +1,19 @@
-﻿internal class Program
+﻿using AppPaiements;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+
+        CarteCredit cartecredit1 = new CarteCredit (50 , "food", 0965);
+        CarteCredit cartecredit2 = new CarteCredit(50, "transport", 0965);
+        CarteCredit cartecredit3 = new CarteCredit(50, "shopping", 0965);
+
+        cartecredit1.AfficherDetails();
+        cartecredit2.AfficherDetails();
+        cartecredit3.AfficherDetails();
+
+
     }
 }
