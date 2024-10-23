a00c43c33319183ad918d13b70e380a7ef436fe8
ydriss foko <300141534@monboreal.ca>
2024-10-23 14:31:15 -0400

    ajout de la classe utilisateur

---
+++ AppPaiements/AppPaiements/Utilisateur.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+    public class Utilisateur
+    {
+        public string Nom { get; set; }
+        public List<string> Paiements { get; set; }
+        public Utilisateur(string nom, List<string> paiments)
+        {
+            Nom = nom;
+            Paiements = Paiements ?? new List<string>();
+        }
+
+        public virtual void AfficherInfos()
+        {
+            Console.WriteLine($"Nom : {Nom}");
+            Console.WriteLine("Paiements :");
+            foreach (var paiement in Paiements)
+            {
+                Console.WriteLine($" * {paiement}");
+            }
+        }
+
+        public virtual void AjouterPaiement(string paiement)
+        {
+            if (!string.IsNullOrEmpty(paiement))
+            {
+                Paiements.Add(paiement);
+                Console.WriteLine($"Montant du paiement : {paiement} ajouté.");
+            }
+            else
+            {
+                Console.WriteLine("Le montant du paiement ne peut pas être vide.");
+            }
+        }
+    }
+}
